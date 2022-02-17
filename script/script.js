4b8e0a502df29f3966e655eaee91e97268e355c0
Salik Akhmetov <sylik3210@gmail.com>
2022-02-17 20:45:00 +0600

    lesson21 Делегирование событий

--- script/script.js
+++ script/script.js
@@ -59,24 +59,25 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // меню
     const toggleMenu = () => {
-
-        const btnMenu = document.querySelector('.menu'),
-            menu = document.querySelector('menu'),
-            closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
+        const menu = document.querySelector('menu');
 
         const handlerMenu = () => {
             menu.classList.toggle('active-menu');
         };
-
-        btnMenu.addEventListener('click', () => {
-            handlerMenu();
+        document.body.addEventListener('click', event => {
+            let target = event.target;
+            if (menu.classList.contains('active-menu')) {
+                let menuArea = target.closest('menu');
+                if (!menuArea || target.classList.contains('close-btn') || target.tagName === 'A') {
+                    handlerMenu();
+                }
+            } else {
+                target = target.closest('.menu');
+                if (target) {
+                    handlerMenu();
+                }
+            }
         });
-        closeBtn.addEventListener('click', () => {
-            handlerMenu();
-        });
-
-        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
     };
     toggleMenu();
 
@@ -84,8 +85,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     const togglePopUp = () => {
         const popup = document.querySelector('.popup'),
-            popupBtn = document.querySelectorAll('.popup-btn'),
-            popupClose = document.querySelector('.popup-close');
+            popupBtn = document.querySelectorAll('.popup-btn');
 
         // popup animation
         const popupAnim = (t) => {
@@ -110,8 +110,17 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }));
 
-        popupClose.addEventListener('click', () => {
-            popup.style.display = 'none';
+        popup.addEventListener('click', event => {
+            let target = event.target;
+            if (target.classList.contains('popup-close')) {
+                popup.style.display = 'none';
+            } else {
+                target = target.closest('.popup-content');
+
+                if (!target) {
+                    popup.style.display = 'none';
+                }
+            }
         });
     };
     togglePopUp();
@@ -148,4 +157,38 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     scrollTo();
+
+    // Табы
+
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
+        
+        const toggleTabContent = index => {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+        };
+
+        tabHeader.addEventListener('click', event => {
+            let target = event.target;
+            target = target.closest('.service-header-tab');
+            if (target) {
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
+                });
+            }
+        });
+    };
+
+    tabs();
 });
