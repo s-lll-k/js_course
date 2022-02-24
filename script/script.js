a382e9c37b9aca0f49b1491b9f2273523b34b5d7
s-lll-k <sylik3210@gmail.com>
2022-02-24 14:29:15 +0600

    lesson26 AJAX XHR

--- script/script.js
+++ script/script.js
@@ -400,4 +400,56 @@ window.addEventListener('DOMContentLoaded', function () {
 
     calc();
 
+    // send-ajax-form
+    const sendForm = () => {
+        const errorMessage = 'Что то пошло не так...',
+            loadMessage = 'Загрузка...',
+            successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+        const form = document.querySelector('#form1');
+
+        const statusMessage = document.createElement('div');
+        statusMessage.style.cssText = 'font-size: 2rem;'
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+            const formData = new FormData(form);
+            let body = {};
+
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+            postData(body, () => {
+                statusMessage.textContent = successMessage; 
+            }, (error) => {
+                statusMessage.textContent = errorMessage;
+                console.log(error);
+            });
+
+        });
+
+        const postData = (body, outputData, errorData) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+
+                if (request.readyState !== 4) {
+                    return;
+                }
+
+                if (request.status === 200) {
+                    outputData();
+                } else {
+                    errorData(request.status);
+                }
+            });
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'applicaion/json');
+            
+
+            request.send(JSON.stringify(body));
+        }
+    };
+    sendForm();
 });
